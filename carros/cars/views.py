276f98531d693d85501884f5c26df9c0708c967a
Guilheme Castilho <guilherme.castilhot2c18@gmail.com>
2023-07-21 20:05:30 -0300

    Update views.py

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 from cars.models import Car
+
 
 # Create your views here.
 def cars_view(request):
+    cars = Car.objects.all().order_by("model")
+
+    search = request.GET.get("search")
+
+    if search:
+        cars = cars.filter(model__icontains=search)
+
     cars = Car.objects.all()
     return render(request, "cars.html", {"cars": cars})
