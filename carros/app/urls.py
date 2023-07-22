c1ea55da1caa40e3530fb165dc03987932bd206f
Guilheme Castilho <guilherme.castilhot2c18@gmail.com>
2023-07-22 17:45:14 -0300

    cars.html

--- carros/app/urls.py
+++ carros/app/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from cars.views import cars_view
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [path("admin/", admin.site.urls), path("cars/", cars_view)] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+from cars.views import cars_view
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("cars/", cars_view, name="cars_list"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
