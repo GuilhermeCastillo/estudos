f5d0404c2f4d839d7493e19bd00e3ec4e4902607
Guilheme Castilho <guilherme.castilhot2c18@gmail.com>
2023-07-29 12:53:43 -0300

    accounts

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from cars.models import Car
+from cars.forms import CarModelForm
 
 
 # Create your views here.
@@ -13,5 +14,17 @@ def cars_view(request):
     if search:
         cars = cars.filter(model__icontains=search)
 
-    cars = Car.objects.all()
     return render(request, "cars.html", {"cars": cars})
+
+def new_car_view(request):
+    
+    if request.method == "POST":
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if  new_car_form.is_valid():
+            new_car_form.save()
+            return redirect("cars_list")
+    else:
+        new_car_form = CarModelForm()
+    
+    print(request)
+    return render(request, "new_car.html", {"new_car_form": new_car_form})
