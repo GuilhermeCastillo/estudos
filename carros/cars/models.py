e1f615ca54795dd2510ee6e4cb3c422ce9de37d0
Guilheme Castilho <guilherme.castilhot2c18@gmail.com>
2023-07-30 10:21:50 -0300

    signals

--- carros/cars/models.py
+++ carros/cars/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    
+
     def __str__(self):
-        return self.name    
+        return self.name
+
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
@@ -19,3 +21,15 @@ class Car(models.Model):
 
     def __str__(self):
         return self.model
+
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.cars_count} - {self.cars_value}"
